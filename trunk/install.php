1ddfdf747790a16c329b3c7defb53900495d29ca
migan <migan@39c2e608-8808-0410-902f-c6fd42832a1e>
2006-01-02 21:22:26 +0000

    Bugfix in installer

--- trunk/install.php
+++ trunk/install.php
@@ -17,7 +17,7 @@ $_SESSION['murrix']['site'] = "standard";
 $_SESSION['murrix']['languages'] = "eng";
 
 if (!isset($_SESSION['murrix']['System']))
-	$_SESSION['murrix']['System'] = new mSystem($_SERVER['REQUEST_URI']);
+	$_SESSION['murrix']['System'] = new mSystem($_SERVER['REQUEST_URI']."install.php");
 
 $_SESSION['murrix']['System']->LoadScripts();
 $_SESSION['murrix']['System']->Process();
