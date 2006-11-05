b24a978b591f4e9d73882edd449f052367bbb7d0
migan <migan@39c2e608-8808-0410-902f-c6fd42832a1e>
2006-11-05 19:22:30 +0000

    svn sucks

--- trunk/classes/class.mform.php
+++ trunk/classes/class.mform.php
@@ -1,1 +1,47 @@
- 
+<?
+
+class mForm
+{
+	var $name;
+	
+	function mForm($name)
+	{
+		$this->name = $name;
+	}
+	
+	function begin($target)
+	{?>
+		<form name="<?=$this->name?>" id="<?=$this->name?>" action="javascript:void(null);" onsubmit="Post('<?=$target?>','<?=$this->name?>')">
+	<?}
+	
+	function end()
+	{?>
+		</form>
+	<?}
+	
+	function input($name, $args)
+	{
+		$string = "";
+		foreach ($args as $key => $value)
+			$string .= " $key=\"$value\"";
+		?>
+		<input name="<?=$name?>" id="<?=$name?>"<?=$string?>/>
+	<?}
+	
+	function select($name, $args, $list)
+	{
+		$string = "";
+		foreach ($args as $key => $value)
+			$string .= " $key=\"$value\"";
+		?>
+		<select name="<?=$name?>" id="<?=$name?>"<?=$string?>/>
+		
+		</select>
+	<?}
+<select class="selectbox" name="instantthumbs">
+		<option <?=("true" == $args['instantthumbs'] ? "selected" : "")?> value="true">Yes</option>
+		<option <?=("false" == $args['instantthumbs'] ? "selected" : "")?> value="false">No</option>
+	</select>
+}
+
+?>
